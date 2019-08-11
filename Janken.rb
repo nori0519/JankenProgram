ce0002e318ab03acf7ff21dfecf19ccbdc66529c
unknown <tanno.noritaka.infratop@gmail.com>
2019-08-11 13:17:36 +0900

    [Fix2]

--- Janken.rb
+++ Janken.rb
@@ -5,15 +5,13 @@ puts '[1]:チョキ'
 puts '[2]:パー'
 janken={0=>"グー",1=>"チョキ",2=>"パー"}
 my_hand =gets.to_i
-if my_hand<0 && my_hand>2
-	puts '入力された値が無効です。'
-end
 his_hand=rand(0..2)
+
 if my_hand==0
 	puts "あなたの手:#{janken[0]},相手の手：#{janken[his_hand]}"
 elsif my_hand==1
 	puts "あなたの手:#{janken[1]},相手の手：#{janken[his_hand]}"
-elsif
+elsif my_hand==2
 	puts "あなたの手:#{janken[2]},相手の手：#{janken[his_hand]}"
 else
 	puts "入力された値が無効です。"
