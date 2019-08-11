dd4b6b448efacd46df7d199a613c18a927e72248
unknown <tanno.noritaka.infratop@gmail.com>
2019-08-11 11:12:02 +0900

    [Finish]

---
+++ Janken.rb
@@ -0,0 +1,44 @@
+puts 'じゃんけんを開始します。'
+puts '最初はグー、じゃんけん...'
+puts '[0]:グー'
+puts '[1]:チョキ'
+puts '[2]:パー'
+janken={0=>"グー",1=>"チョキ",2=>"パー"}
+my_hand =gets.to_i
+if my_hand<0 && my_hand>2
+	puts '入力された値が無効です。'
+end
+his_hand=rand[0..2]
+if my_hand==0
+	puts "あなたの手:#{janken[0]},相手の手：#{his_hand}"
+elsif my_hand==1
+	puts "あなたの手:#{janken[1]},相手の手：#{his_hand}"
+else
+	puts "あなたの手:#{janken[2]},相手の手：#{his_hand}"
+end
+if my_hand==0
+	if his_hand==0
+		puts '引き分けです。'
+	elsif his_hand==1
+		puts 'あなたの勝ちです。'
+	else
+		puts 'あなたの負けです。'
+	end
+elsif my_hand==1
+	if his_hand==0
+		puts 'あなたの負けです。'
+	elsif his_hand==1
+		puts '引き分けです。'
+	else
+		puts 'あなたの勝ちです。'
+	end
+else my_hand==2
+	if his_hand==0
+		puts 'あなたの勝ちです。'
+	elsif his_hand==1
+		puts 'あなたの負けです。'
+	else
+		puts '引き分けです。'
+	end
+end
+puts'じゃんけんを終了します。'
